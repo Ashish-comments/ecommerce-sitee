cdf4ca79e29d5f2d13db77d4baf84c2baddf1235
Ashish-comments <dwivedi18ashish18@gmail.com>
2025-06-22 23:12:48 -0700

    Update route.js

---
+++ route.js
@@ -0,0 +1,23 @@
+{
+  "products" [
+    {
+      "id": 1,
+      "name": "Smart Watch",
+      "price": 2500,
+      "image": "watch.jpg",
+      "description": "Digital smart watch with health tracker"
+    },
+    {
+      "id": 2,
+      "name": "Wireless Earbuds",
+      "price": 1500,
+      "image": "earbuds.jpg",
+      "description": "High-quality wireless earbuds"
+    }
+  ],
+  "users" ,
+  "orders" 
+}
+
+
+
